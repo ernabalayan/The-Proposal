95e5b7d8a84c3acda3286562018d525c9f25c4eb
ShadowCat567 <lv2067@nyu.edu>
2022-11-28 22:53:49 -0500

    post processing adjustments

--- Assets/Proposal/Scripts/PlayerMovement.cs
+++ Assets/Proposal/Scripts/PlayerMovement.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -12,14 +14,20 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody rb;
 
     Vector3 moveDir;
+    //Vector3 driftDir;
 
     int alcoholContent = 0;
 
     Vector3 drift = Vector3.zero;
-    Vector3 driftVect = new Vector3(0.0f, 0.0f, 0.6f);
+    Vector3 driftVect = new Vector3(0.0f, 0.0f, 0.7f);
     float driftTimer;
     float changeDirection = 2.0f;
     bool setDriftVector = false;
+
+    //GameObject PPvol;
+    [SerializeField] Volume rendPP;
+    ChromaticAberration ca;
+    PaniniProjection pp;
 
     // Start is called before the first frame update
     void Start()
@@ -28,11 +36,17 @@ public class PlayerMovement : MonoBehaviour
         pi = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        //rendPP = GetComponent<Volume>();
+        //use this to figure out changing stuff through code: https://forum.unity.com/threads/how-to-modify-post-processing-profiles-in-script.758375/
+        rendPP.profile.TryGet(out ca);
+        rendPP.profile.TryGet(out pp);
+        ca.intensity.value = 0.0f;
+        rendPP.enabled = false;
     }
 
     private void Update()
     {
-        if(alcoholContent > 1)
+        if(alcoholContent > 1 && alcoholContent <= 2)
         {
             //Debug.Log("drifting");
             if(!setDriftVector)
@@ -49,6 +63,12 @@ public class PlayerMovement : MonoBehaviour
                 //Debug.Log("Changing direction");
                 drift *= -1;
             }
+        }
+        else if(alcoholContent > 2)
+        {
+            rendPP.enabled = true;
+            ca.intensity.value = 0.5f;
+            pp.distance.value = 0.3f;
         }
     }
 
