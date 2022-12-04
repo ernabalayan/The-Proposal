5d73fd378e4595feb9af29f500b6c09c03ccd838
ernabalayan <erna2001balayan@gmail.com>
2022-12-04 09:26:41 -0500

    updated alcohol bottles and key

---
+++ Assets/KeyCollider.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCollider : MonoBehaviour
+{
+    [SerializeField] GameObject bathroomDoor;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Touching Key");
+            bathroomDoor.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+}
--- Assets/Proposal/Scripts/MouseLook.cs
+++ Assets/Proposal/Scripts/MouseLook.cs
@@ -14,7 +14,7 @@ public class MouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        yRotation = playerTransf.eulerAngles.y;
     }
 
     // Update is called once per frame
