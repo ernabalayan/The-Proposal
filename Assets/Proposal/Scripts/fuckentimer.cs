602c6f3e2b60458cc4d33ff9923b1f390e51e71b
Kris <cmc9342@nyu.edu>
2022-12-13 03:18:41 -0500

    idk

---
+++ Assets/Proposal/Scripts/fuckentimer.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class fuckentimer : MonoBehaviour
+{
+    public float maxTime;
+    float time;
+
+    TextMeshProUGUI textmeshproText;
+    string objectiveTimer;
+    int timeDisplay;
+
+    void Awake()
+    {
+        time = maxTime;
+    }
+
+    void Start()
+    {
+        textmeshproText = this.gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        timeDisplay = Mathf.RoundToInt(time);
+        objectiveTimer = timeDisplay.ToString();
+        textmeshproText.text = "Time Left: " + objectiveTimer;
+    }
+
+    void FixedUpdate()
+    {
+        if(time > 0) 
+        {
+            time -= Time.deltaTime;
+            if(time < 0) time = 0;
+            return;
+        }
+
+        if(time == 0)
+        {
+            Debug.Log("Change to failed scene");
+        }
+    }
+}
